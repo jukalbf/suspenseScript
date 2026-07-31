392fa625cfd938a6acb1f49e7f8c60ebccf2c5f4
jukalbf <lgbr212@gmail.com>
2026-07-31 15:48:51 -0300

    feat: Util para inserir arquivo de estilos vazio

---
+++ utils/writeStylesFile.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func WriteStylesFile(parentFolder string) {
+	fileContent := "// Arquivo de estilos.\n// Se precisar, copie e cole o conteúdo aqui.\n// Se não, apenas delete esse arquivo."
+	path := fmt.Sprintf("%s/styles.module.scss", parentFolder)
+	err := os.WriteFile(path, []byte(fileContent), 0776)
+	CheckErr(err)
+}
