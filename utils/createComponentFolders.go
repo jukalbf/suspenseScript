af33c863fc4f8415f2e0e80c8a61501e8146f574
jukalbf <lgbr212@gmail.com>
2026-07-31 15:56:49 -0300

    feat: Util para criar pastas dos componentes

---
+++ utils/createComponentFolders.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+// Cria as pastas dos componentes.
+func CreateComponentFolders(filesDir []string) (componentsDir []string) {
+	fmt.Println("=> Criando pastas para o componentes...")
+
+	for _, dir := range filesDir {
+		fullFolderName := GetParentFolderName(dir)
+		parentDir := fmt.Sprintf("src/components/%s", fullFolderName)
+		os.Mkdir(parentDir, os.ModePerm)
+		componentsDir = append(componentsDir, parentDir)
+	}
+
+	fmt.Println("=> Pastas criadas.")
+
+	return componentsDir
+}
