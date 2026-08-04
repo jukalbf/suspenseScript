218d7b89e687c17812e73c5ad289b4822d25c821
jukalbf <lgbr212@gmail.com>
2026-08-04 15:44:14 -0300

    style: Alt nome de variável e add desc

--- utils/placeComponent.go
+++ utils/placeComponent.go
@@ -5,8 +5,9 @@ import (
 	"strings"
 )
 
-func PlaceComponent(componentFolder string, line *string) {
+// Implementa o novo componente no arquivo original.
+func PlaceComponent(componentFolder string, targetRow *string) {
 	extractedFolderName := ExtractParentFolder(componentFolder)
 	formatedTagName := fmt.Sprintf("%s%s", strings.ToUpper(extractedFolderName[:1]), extractedFolderName[1:])
-	*line = fmt.Sprintf("\t\t\t\t<%s />\n", formatedTagName)
+	*targetRow = fmt.Sprintf("\t\t\t\t<%s />\n", formatedTagName)
 }
