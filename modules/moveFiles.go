bb8b8a1880879e0993f7e68fabd75ed090895251
jukalbf <lgbr212@gmail.com>
2026-08-04 14:54:40 -0300

    style: Enxugando código
    - Passando lógica para funções externas
    - Implementando novas funções

--- modules/moveFiles.go
+++ modules/moveFiles.go
@@ -4,6 +4,7 @@ import (
 	"main/utils"
 )
 
+// Modulo para mover conteudo dos arquivos para as pastas dos componentes.
 func MoveFiles(path string) (filesDir []string, componentsFolders []string) {
 	dirNames := utils.ReadDir(path)
 	targetFiles, filesDir := utils.ExtractFiles(dirNames)
