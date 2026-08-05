ed4f15330dfdefbd821d5bcc7b2a1c55a3901cec
jukalbf <lgbr212@gmail.com>
2026-08-05 10:21:21 -0300

    feat: Adicionando tag de Suspense

--- utils/placeComponent.go
+++ utils/placeComponent.go
@@ -9,5 +9,5 @@ import (
 func PlaceComponent(componentFolder string, targetRow *string) {
 	extractedFolderName := ExtractParentFolder(componentFolder)
 	formatedTagName := fmt.Sprintf("%s%s", strings.ToUpper(extractedFolderName[:1]), extractedFolderName[1:])
-	*targetRow = fmt.Sprintf("\t\t\t\t<%s />\n", formatedTagName)
+	*targetRow = fmt.Sprintf("\t\t\t\t<Suspense fallback={Loading}/>\n\t\t\t\t\t<%s />\n\t\t\t\t<Suspense fallback={Loading}/>\n", formatedTagName)
 }
